380bd3ad41313a3f805502b9af569d1097e69be9
pltoledo <pedroltoledo@gmail.com>
2026-02-05 23:22:22 -0300

    feat: add resolver module declaration file

---
+++ src/resolvers/externals.d.ts
@@ -0,0 +1,5 @@
+declare module "@aws-sdk/client-secrets-manager";
+declare module "@azure/keyvault-secrets";
+declare module "@azure/identity";
+declare module "@google-cloud/secret-manager";
+declare module "@1password/sdk";
